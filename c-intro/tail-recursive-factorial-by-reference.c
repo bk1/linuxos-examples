fe84fed84b34530f859ab9b69da4a551dc91700d
Karl Brodowsky <bk1@gmx.net>
2014-09-08 20:06:48 +0200

    by reference

---
+++ c-intro/tail-recursive-factorial-by-reference.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int fac(int n, int *result_ptr);
+int fac_intern(int n, int *result_ptr);
+
+int main(int argc, char *argv[]) {
+  int i;
+  int n;
+  for (n = 0; n < 20; n++) {
+    int f;
+    int r = fac(n, &f);
+    printf("r=%02d n=%10d f=%10d\n", r, n, f);
+  }
+  for (i = 0, n = 0; i < 1000; i++, n = 2*n+1) {
+    int f;
+    int r = fac(n, &f);
+    printf("i=%4d r=%02d n=%10d f=%10d\n", i, r, n, f);
+  }
+  return 0;
+}
+
+int fac(int n, int *result_ptr) {
+  *result_ptr = 1;
+  return fac_intern(n, result_ptr);
+}
+
+int fac_intern(int n, int *result_ptr) {
+  if (n < 0) {
+    *result_ptr = 0;
+    return -1;
+  } else if (*result_ptr == 0) {
+    return 0;
+  } else if (n <= 1) {
+    return 0;
+  } else {
+    *result_ptr *= n;
+    return fac_intern(n - 1, result_ptr);
+  }
+}
+
